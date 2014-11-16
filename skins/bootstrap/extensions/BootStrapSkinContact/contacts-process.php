acbaf5dda427ff05d5fc0bd4777a63526c33b371
qingji.chen <qingji.chen@chukong-inc.com>
2014-11-16 22:20:47 +0800

    tongshang

--- skins/bootstrap/extensions/BootStrapSkinContact/contacts-process.php
+++ skins/bootstrap/extensions/BootStrapSkinContact/contacts-process.php
@@ -7,13 +7,13 @@ $subject = '来自mediawikibootstrapskin.9ong.com的信息';
 if(!preg_match('/^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/',$_POST['email']))
 die('哎呀，邮箱地址可能不合法啦!');
 
-$headers = 'From: ' . $_POST['email'] . "\r\n" . 'Reply-To: ' . $_POST['email'];
+$headers = 'From: ' . $_POST['email'] . "\n" . 'Reply-To: ' . $_POST['email'];
 
 $message = 'Name: ' . $_POST['name'] . "\n" .
  'E-mail: ' . $_POST['email'] . "\n" .
  'Subject: ' . $_POST['subject'] . "\n" .
  'Message: ' . $_POST['message'];
-
+file_put_contents("/tmp/bs.mail.log", $headers."\n".$message,FILE_APPEND);
 mail($to, $subject, $message, $headers);
 
 }
