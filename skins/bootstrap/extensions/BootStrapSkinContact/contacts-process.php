bba9342bb7547f4158aa15bbdd1757df17e971a6
qingji.chen <qingji.chen@chukong-inc.com>
2014-11-16 22:42:44 +0800

    完善邮件发送给中文编码

--- skins/bootstrap/extensions/BootStrapSkinContact/contacts-process.php
+++ skins/bootstrap/extensions/BootStrapSkinContact/contacts-process.php
@@ -2,18 +2,24 @@
 if( isset($_POST['name']) )
 {
 $to = 'jm-cqj@163.com';
-$subject = '来自mediawikibootstrapskin.9ong.com的信息';
+$subject = $_POST['subject'];
+$subject = "=?UTF-8?B?".base64_encode($subject)."?=";//中文主题处理
 
 if(!preg_match('/^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/',$_POST['email']))
 die('哎呀，邮箱地址可能不合法啦!');
 
-$headers = 'From: ' . $_POST['email'] . "\n" . 'Reply-To: ' . $_POST['email'];
+/**
+ * 中文header处理
+ */
+$headers = 'MIME-Version: 1.0' . "\r\n";  
+$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+$headers .= 'From: ' . $_POST['email'] . "\r\n" . 'Reply-To: ' . $_POST['email'];
 
-$message = 'Name: ' . $_POST['name'] . "\n" .
- 'E-mail: ' . $_POST['email'] . "\n" .
- 'Subject: ' . $_POST['subject'] . "\n" .
- 'Message: ' . $_POST['message'];
-file_put_contents("/tmp/bs.mail.log", $headers."\n".$message,FILE_APPEND);
+$message = 'Name: ' . $_POST['name'] . "<br />" .
+ 'E-mail: ' . $_POST['email'] . "<br />" .
+ 'Subject: ' . $_POST['subject'] . "<br />" .
+ 'Message: ' . $_POST['message']."<br />";
+
 mail($to, $subject, $message, $headers);
 
 }
