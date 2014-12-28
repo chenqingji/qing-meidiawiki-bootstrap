a6aee3d53b815dfc69ae1e678e8491d452371b9b
qingji.chen <qingji.chen@chukong-inc.com>
2014-12-28 21:11:05 +0800

    add share plugin

--- skins/bootstrap/bootstrapskin.php
+++ skins/bootstrap/bootstrapskin.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Initialisation file for the BootStrapSkin
  *
@@ -10,51 +11,51 @@
  * @ingroup Skins
  * @authors Lee Miller
  */
+if (!defined('MEDIAWIKI'))
+        die("This is an extension to the MediaWiki package and cannot be run standalone.");
 
-if( !defined( 'MEDIAWIKI' ) ) die( "This is an extension to the MediaWiki package and cannot be run standalone." );
- 
 $wgExtensionCredits['skin'][] = array(
-        'path' => __FILE__,
-        'name' => 'BootStrapSkin',
-        'url' => "http://www.mediawikibootstrapskin.co.uk/",
-        'author' => 'Lee Miller and [http://www.vanhamel.nl/wiki/Main_page Dennis Groenewegen]',
-        'descriptionmsg' => 'bootstrapskin-desc',
-		'version' => '1.0.43',
+    'path' => __FILE__,
+    'name' => 'BootStrapSkin',
+    'url' => "http://www.mediawikibootstrapskin.co.uk/",
+    'author' => 'Lee Miller and [http://www.vanhamel.nl/wiki/Main_page Dennis Groenewegen]',
+    'descriptionmsg' => 'bootstrapskin-desc',
+    'version' => '1.0.43',
 );
 
 $wgValidSkinNames['bootstrapskin'] = 'Bootstrap';
-$wgAutoloadClasses['SkinBootstrap'] = dirname(__FILE__).'/Bootstrapskin.skin.php';
-$wgExtensionMessagesFiles['SkinBootstrap'] = dirname(__FILE__).'/Bootstrapskin.i18n.php';
- 
-$wgResourceModules['skins.bootstrapskin'] = array(
-        'styles' => array(
-		        'bootstrap/bootstrap/less/bootstrap.less' => array( 'media' => 'screen' ),
-                'bootstrap/bootstrap/css/bootstrap-blue.css' => array( 'media' => 'screen' ),
-                'bootstrap/screen.css' => array( 'media' => 'screen' ),
-                'bootstrap/theme.css' => array( 'media' => 'screen' ),
-				'bootstrap/bootstrap/css/jquery-ui.min.css' => array( 'media' => 'screen' ),
-				'bootstrap/bootstrap/css/font.css' => array( 'media' => 'screen' ),
-				'bootstrap/bootstrap/css/jquery-ui.min.css' => array( 'media' => 'screen' ),
-				//'bootstrap/bootstrap/css/glyphicons.css' => array( 'media' => 'screen' ),
-				//'bootstrap/bootstrap/css/fam-icons.css' => array( 'media' => 'screen' ),
-                'bootstrap/bootstrap/css/typography.css' => array( 'media' => 'screen' ),
-                'bootstrap/bootstrap/css/misc.css' => array( 'media' => 'screen' ),
-                //'bootstrap/bootstrap/css/social-buttons.css' => array( 'media' => 'screen' ),
-				//'bootstrap/bootstrap/css/prettify.css' => array( 'media' => 'screen' ),
-	),
+$wgAutoloadClasses['SkinBootstrap'] = dirname(__FILE__) . '/Bootstrapskin.skin.php';
+$wgExtensionMessagesFiles['SkinBootstrap'] = dirname(__FILE__) . '/Bootstrapskin.i18n.php';
 
-	'scripts' => array(
-	    'bootstrap/bootstrap/js/jquery-ui.min.js',
-		'bootstrap/bootstrap/js/bootstrap.js',
-		'bootstrap/skin.js',
-		'bootstrap/bootstrap/js/buttons.js',
+$wgResourceModules['skins.bootstrapskin'] = array(
+    'styles' => array(
+        'bootstrap/bootstrap/less/bootstrap.less' => array('media' => 'screen'),
+        'bootstrap/bootstrap/css/bootstrap-blue.css' => array('media' => 'screen'),
+        'bootstrap/screen.css' => array('media' => 'screen'),
+        'bootstrap/theme.css' => array('media' => 'screen'),
+        'bootstrap/bootstrap/css/jquery-ui.min.css' => array('media' => 'screen'),
+        'bootstrap/bootstrap/css/font.css' => array('media' => 'screen'),
+        'bootstrap/bootstrap/css/jquery-ui.min.css' => array('media' => 'screen'),
+        //'bootstrap/bootstrap/css/glyphicons.css' => array( 'media' => 'screen' ),
+        //'bootstrap/bootstrap/css/fam-icons.css' => array( 'media' => 'screen' ),
+        'bootstrap/bootstrap/css/typography.css' => array('media' => 'screen'),
+        'bootstrap/bootstrap/css/misc.css' => array('media' => 'screen'),
+    //'bootstrap/bootstrap/css/social-buttons.css' => array( 'media' => 'screen' ),
+    //'bootstrap/bootstrap/css/prettify.css' => array( 'media' => 'screen' ),
+    ),
+    'scripts' => array(
+        'bootstrap/bootstrap/js/jquery-ui.min.js',
+        'bootstrap/bootstrap/js/bootstrap.js',
+        'bootstrap/skin.js',
+        'bootstrap/bootstrap/js/buttons.js',
         'bootstrap/bootstrap/js/portlets.js',
-		'bootstrap/bootstrap/js/misc.js',
+        'bootstrap/bootstrap/js/misc.js',
         'bootstrap/bootstrap/js/tab.js',
-      //'bootstrap/bootstrap/js/run_prettify.js',	  
-	),	
-        'remoteBasePath' => &$GLOBALS['wgStylePath'],
-        'localBasePath' => &$GLOBALS['wgStyleDirectory'],
+        'bootstrap/bootstrap/js/share.min.js',
+    //'bootstrap/bootstrap/js/run_prettify.js',	  
+    ),
+    'remoteBasePath' => &$GLOBALS['wgStylePath'],
+    'localBasePath' => &$GLOBALS['wgStyleDirectory'],
 );
 
 # Default options to customise skin
@@ -64,16 +65,16 @@ $wgBootstrapSkinAnonNavbar = true;
 $wgBootstrapSkinUseStandardLayout = false;
 $wgBootstrapSkinDisplaySidebarNavigation = false;
 # Show print/export in navbar by default
-$wgBootstrapSkinSidebarItemsInNavbar = array( 'coll-print_export' );
+$wgBootstrapSkinSidebarItemsInNavbar = array('coll-print_export');
 
 $wgFooterIcons['poweredby']['bootstrapskin'] = array(
-	      "src" => "http://www.mediawikibootstrapskin.co.uk/images/BootStrapSkin_mediawiki_88x31.png",
-	      "url" => "http://www.mediawikibootstrapskin.co.uk/",
-	      "alt" => "Powered by BootStrapSkin",
-          );
-		  
+    "src" => "http://www.mediawikibootstrapskin.co.uk/images/BootStrapSkin_mediawiki_88x31.png",
+    "url" => "http://www.mediawikibootstrapskin.co.uk/",
+    "alt" => "Powered by BootStrapSkin",
+);
+
 $wgFooterIcons['poweredby']['youricon'] = array(
-	      "src" => "", //url to your wiki logo
-	      "url" => "", //url to your wiki
-	      "alt" => "", //alt text
-          );
+    "src" => "", //url to your wiki logo
+    "url" => "", //url to your wiki
+    "alt" => "", //alt text
+);
